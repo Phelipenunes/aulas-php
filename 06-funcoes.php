00232e7d5b1ce55120993d89478d357a281445c1
Phelipe nunes <phelipesenac@gmail.com>
2023-08-03 09:37:53 -0300

    Funções com parametros
    Funções com parametros (Exemplos)

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -30,11 +30,46 @@
     <h2>Funções com retorno de dados </h2>
     <?php
     function dadoscurso(){
-        return "Técnico em informatica para internet";
+        return "<b>Técnico em informatica para internet</b>";
     }
     ?>
     <p> estamos no curso de <?=dadoscurso()?>.</p>
     <p><?=dadoscurso()?> é ministrado no senac penha.</p>
+
+    <hr>
+    <h3>Funções com parametro (ou argumentos)</h3>
+    <?php
+    function soma($valor1, $valor2, $valor3){
+        $total = $valor1 + $valor2 + $valor3;
+        return $total;
+    }
+    ?>
+    <p>resultado 1: <?= soma( 10, 20, 5)?></p>
+    <p>resultado 2: <?= soma( 23, 22, 50)?></p>
+    <?php
+    $resultado3 = soma(500, 580, 500);
+    ?>
+    <p> Resultado 3:<?=$resultado3?></p>
+    <?php if($resultado3 > 100 ){?>
+        <p>Está demitido!</p>
+    <?php
+    }   
+    ?>
+    <?php if( soma(2, 1, 5) <= 10){?>
+        <p> Está reprovado!</p>
+    <?php } ?>
+
+    <?php 
+    function saudacao($mensagem, $pessoa = "fulano(a)"){
+    return "Olá, $mensagem $pessoa ";
+    }
+    ?>
+        <p> <?=saudacao("bom dia","Phelipe")?></p>
+        <p> <?=saudacao("bom dia","Vitor")?></p>
+        <p> <?=saudacao("bom dia")?></p>
+
+
+
 
 
 </body>
